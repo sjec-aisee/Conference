973f75b9af6bd47104e524abe8489d68d32b2064
Pratham-U-dev <pratham.u05@gmail.com>
2026-01-27 22:30:59 +0530

    added travel/Explore

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,14 @@
 document.querySelector('.nav-toggle').addEventListener('click', () => {
     document.querySelector('.nav-links').classList.toggle('open');
 });
+// Back to Top Button Logic
+const backToTopBtn = document.getElementById('backToTop');
+
+if(backToTopBtn){
+    backToTopBtn.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+}
